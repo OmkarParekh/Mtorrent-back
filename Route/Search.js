2a7fed7f91e8323535b8e387cc6e169766444df1
Omkar Parekh <omkarparekh600@gmail.com>
2022-04-28 20:46:30 +0530

    Proxy Error Solve

--- Route/Search.js
+++ Route/Search.js
@@ -1,10 +1,13 @@
-const router=require('express').Router()
+const router = require("express").Router();
 
-const {Torrent}=require('../Scrapper/Scrapper')
-router.get('/:key',async(req,res)=>{
-     const data=await Torrent(`search/${req.params.key}/1/`)
-     // console.log(data);
-     res.send(data)
-     
-})
-module.exports=router;
+const { Torrent } = require("../Scrapper/Scrapper");
+router.get("/:key", async (req, res) => {
+  try {
+    const data = await Torrent(`search/${req.params.key}/1/`);
+    //     console.log(data);
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+  }
+});
+module.exports = router;
--- Scrapper/Scrapper.js
+++ Scrapper/Scrapper.js
@@ -1,60 +1,65 @@
-const Axios =require('axios')
-const cheerio=require('cheerio')
-async function html(link){
-     const res= await Axios.get(link,{
-          // Proxy example for the Axios Users
-          proxy: {
-               host: `209.127.191.180`,
-               port: 9279,
-               auth: {
-                 username: 'xsdmhtgj-dest',
-                 password: '1bnfxj8vryno'
-               }
-               
-          }   
-          
-     })
-     // console.log(res.data)
-     return cheerio.load(res.data)
-   
-}
-async function Torrent(Route){
-     const torrentdata=[]
-     // console.log(Route);
-     const $ = await html(`https://www.1377x.to/${Route}`)
-     // console.log();
-     $('table  tbody  tr ').each((i,dd)=>{
-          const data={
-               link:$(dd).find('td.coll-1.name a:nth-child(2)').attr('href'),
-               Name:$(dd).find('td.coll-1.name a:nth-child(2)').text(),
-               seed:$(dd).find('td.coll-2.seeds').text(),
-               leeches:$(dd).find('td.coll-3.leeches').text(),
-               time:$(dd).find('td.coll-date').text(),
-               size:$(dd).find('td.coll-4.size.mob-uploader').text(),
-               uploadedby:$(dd).find('td.coll-5.uploader a').text(),
-               
+const Axios = require("axios");
+const cheerio = require("cheerio");
+async function html(link) {
+  const res = await Axios.get(link, {
+    // Proxy example for the Axios Users
+    proxy: {
+      host: `45.140.13.119`,
+      port: 9132,
+      auth: {
+        username: "xsdmhtgj",
+        password: "1bnfxj8vryno",
+      },
+    },
+  });
+  // const res= await Axios.get(link,{
+  //      // Proxy example for the Axios Users
+  //      proxy: {
+  //           host: `209.127.191.180`,
+  //           port: 9279,
+  //           auth: {
+  //             username: 'xsdmhtgj-dest',
+  //             password: '1bnfxj8vryno'
+  //           }
 
-               
-          }
-     torrentdata.push(data)
-     })
-     return(torrentdata)
-    
-    
-// console.log($('table'));
-}
+  //      }
 
-async function magnet(Route){
-     const $ = await html(`https://www.1377x.to/${Route}`)
-     const magnetlink=$('a[href^="magnet:?xt=urn:btih:"]').attr('href')
-     // console.log(magnetlink); 
-     return(magnetlink) 
-    
-    
-// console.log($('table'));
+  // })
+  // console.log(res.data)
+  return cheerio.load(res.data);
+}
+async function Torrent(Route) {
+  const torrentdata = [];
+  // console.log(Route);
+  const $ = await html(`https://www.1377x.to/${Route}`);
+  // console.log();
+  $("table  tbody  tr ").each((i, dd) => {
+    const data = {
+      link: $(dd).find("td.coll-1.name a:nth-child(2)").attr("href"),
+      Name: $(dd).find("td.coll-1.name a:nth-child(2)").text(),
+      seed: $(dd).find("td.coll-2.seeds").text(),
+      leeches: $(dd).find("td.coll-3.leeches").text(),
+      time: $(dd).find("td.coll-date").text(),
+      size: $(dd).find("td.coll-4.size.mob-uploader").text(),
+      uploadedby: $(dd).find("td.coll-5.uploader a").text(),
+    };
+    torrentdata.push(data);
+  });
+  return torrentdata;
+
+  // console.log($('table'));
 }
 
-module.exports={
-     Torrent,
-     magnet
+async function magnet(Route) {
+  const $ = await html(`https://www.1377x.to/${Route}`);
+  const magnetlink = $('a[href^="magnet:?xt=urn:btih:"]').attr("href");
+  // console.log(magnetlink);
+  return magnetlink;
+
+  // console.log($('table'));
 }
+
+module.exports = {
+  Torrent,
+  magnet,
+};
