a9099076a6676308fd7fb90f9b37ea6c5a64f65f
Omkar Parekh <omkarparekh600@gmail.com>
2022-04-28 21:33:44 +0530

    Proxy Error Solve

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-const Express=require('express')
-const app=Express()
-const cors=require('cors')
-var bodyParser = require('body-parser')
-app.use(cors())
-app.use(bodyParser.json())
+const Express = require("express");
+const app = Express();
+const cors = require("cors");
+var bodyParser = require("body-parser");
+app.use(cors());
+app.use(bodyParser.json());
 
+app.use("/Search", require("./Route/Search"));
+app.use("/Magnet", require("./Route/Magnet"));
+app.use("/Trending", require("./Route/Trending"));
+app.use("/Top", require("./Route/Top"));
+app.use("/Popular", require("./Route/Popular"));
 
-app.use('/Search',require('./Route/Search'))
-app.use('/Magnet',require('./Route/Magnet'))
-app.use('/Trending',require('./Route/Trending'))
-app.use('/Top',require('./Route/Top'))
-app.use('/Popular',require('./Route/Popular'))
-
-
-
-app.listen(process.env.PORT || 5000,()=>{
-     console.log('Server has started At port 5000')
-})
+app.listen(process.env.PORT || 8000, () => {
+  console.log("Server has started At port 8000");
+});
 
 // async function data(){
 //      const torrentdata=[]
@@ -30,16 +27,13 @@ app.listen(process.env.PORT || 5000,()=>{
 //      //           time:$(dd).find('td.coll-date').text(),
 //      //           size:$(dd).find('td.coll-4.size.mob-uploader').text(),
 //      //           uploadedby:$(dd).find('td.coll-5.uploader a').text(),
-               
 
-               
 //      //      }
 //      // torrentdata.push(data)
 //      // })
 // //      const $ = await html('https://www.1377x.to/torrent/4720841/Grand-Theft-Auto-V-GTA-V-build-2189-1-52-online-DLCs-MULTi13-DODI-Repack/')
 // //     const magnetlink=$('a[href^="magnet:?xt=urn:btih:"]').attr('href')
 // //     console.log(magnetlink);
-    
+
 // // console.log($('table'));
 // }
-
